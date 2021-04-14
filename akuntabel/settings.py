e6c55d99577b7f92ca3d15dda24a44420270ddeb
ihsanmuafiry <32921393+ihsanmuafiry@users.noreply.github.com>
2021-04-13 17:15:33 -0700

    update

--- akuntabel/settings.py
+++ akuntabel/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y-%9fwtz026*eru+k5gtcs^+em4nelrto7^0e6b$ihpju!dh1y'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
